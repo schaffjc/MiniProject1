f1afef06e81e4f469317a9265d8986d6f1e18a13
schaffjc <schaffjc@mail.uc.edu>
2021-02-16 01:22:09 +0000

    Fixed numTries

--- Main.java
+++ Main.java
@@ -10,12 +10,9 @@ class Main {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     Random r = new Random();
-    int number;
 
     System.out.println("Welcome to the guessing game!");
-    System.out.println("Please enter any positive whole number:"); number = s.nextInt();
-
-    int userNum = number;
+    System.out.println("Please enter any positive whole number:"); int userNum = s.nextInt();
     int randomNum = r.nextInt(userNum);
 
     System.out.println("A random number to guess has been generated");
@@ -25,19 +22,20 @@ class Main {
 
   static void playGame(int theRandomNum, int theUserNum){
     Scanner s2 = new Scanner(System.in);
+    int numTries = 0;
     int userGuess;
     int userNewGuess;
 
     System.out.println("Please guess a number between 0 and " + theUserNum); userGuess = s2.nextInt();
+    numTries++;
 
-    int numTries = 1;
 
-    while (userGuess != theRandomNum) {
-      numTries++;
-      
-      if (userGuess > theRandomNum) {System.out.println("Guess lower!");}
+    while (userGuess != theRandomNum) {      
+      if (userGuess > theRandomNum) {System.out.println("Guess lower!");
+      numTries++;}
 
-      else if(userGuess < theRandomNum) {System.out.println("Guess higher!");}
+      else if(userGuess < theRandomNum) {System.out.println("Guess higher!");
+      numTries++;}
       
       System.out.println("Enter your new guess: "); userGuess = s2.nextInt();}
 
